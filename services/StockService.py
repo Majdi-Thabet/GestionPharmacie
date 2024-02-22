645a255f8e8e57631edde91cb530ff3cb2114903
Mejdi Thabet <majdi.thabet1997@gmail.com>
2024-02-22 11:55:44 +0100

    updates

--- services/StockService.py
+++ services/StockService.py
@@ -1,58 +1,28 @@
 import models.Pharmacie
-
-    
 class StockService:
-    c =[]
-    def main():
-        while True:
-            print("1. INSERT")
-            print("2. DISPLAY ")
-            print("3. SEARCH")
-            print("4. DELETE")
-            print("5. UPDATE")
-            print("0. EXIT")
+    
+    def menu(self):
+        print("1: Ajouter Produit\n 2: Supprimer produit\n 3: Modifier détails Produit\n 4: Afficher Produit\n 5:Quitter\n")
+    option = int(input("Entrer votre numero d'option"))
+    
+    if option == 1:
+        self.ajouterproduit()
+        
+        
+        
+    # ajouter produit
+    #def ajouter_produit(self, produit):
+    #nom, prix, categorie, fournisseur, quantité, reference, date_expiration
+    p1 = Produit("Lait_bébé", 15, "Lait", "AAAA", 10, 001, 11/12/2024)
 
-            # Controle saisie avec Try Catch
-            try:
-                ch = int(input("Enter Your Choice: "))
+    # rechercher produit
+    def supprimer_produit(self, produit):
+        u
 
-                if ch == 0:
-                    break  # Sortie de la boucle si l'utilisateur choisit 0
+    # afficher produit
+    def afficher_produit(self, produit):
+        hh
 
-                # Ajoutez ici la logique pour chaque choix (1 à 5)
-                if ch == 1:
-                    # Logique pour l'option INSERT
-                    print("Enter Empno: ", end="")
-                    #lire l'entrée de l'utilisateur
-                    eno = int(input())
-                    print("Enter EmpName: ", end="")
-                    ename = input()
-                    print("Enter Salary: ", end ="")
-                    salary = int(input())
-                    CRUDDemo.c.append(Employee(eno, ename, salary))
-                    
-                    # Je veux afficher un msg après l'insertion
-                    # print('--------------------------')
-
-                elif ch == 2:
-                    # Logique pour l'option DISPLAY
-                    pass
-                elif ch == 3:
-                    # Logique pour l'option SEARCH
-                    pass
-                elif ch == 4:
-                    # Logique pour l'option DELETE
-                    pass
-                elif ch == 5:
-                    # Logique pour l'option UPDATE
-                    pass
-                else:
-                    print("Invalid choice. Please enter a number between 0 and 5.")
-
-            except ValueError:
-                print("Invalid input. Please enter a valid integer.")
-
-if __name__ == "__main__":
-    CRUDDemo.main()
-
-
+    # modifier produit
+    def modifier_produit(self, produit):
+        j
